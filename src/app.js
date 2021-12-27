2860c0ba0388099a0f9862774cfecb0ee314ce5a
inksp0ts09 <maryanne.mcglone09@gmail.com>
2021-12-27 11:16:26 -0500

    Corrected apiUrl

--- src/app.js
+++ src/app.js
@@ -71,8 +71,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "c331439fb9ce2dda219db0c33143aaec";
-  let apiUrl = `htpps://api.openweathermap.org/data/2.5/onecall?
-  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
