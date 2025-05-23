66c7c510984b6853b223691abaef233a533cfc67
inksp0ts09 <92317108+inksp0ts09@users.noreply.github.com>
2025-05-23 14:16:34 -0400

    added new api key

--- src/app.js
+++ src/app.js
@@ -70,7 +70,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "c331439fb9ce2dda219db0c33143aaec";
+  let apiKey = "7db1df5b20a3080693ca7764a3471920";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
